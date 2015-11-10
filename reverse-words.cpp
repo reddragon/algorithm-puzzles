521e92b009f72a4fc3c1c0308e412be3d36f87cf
Gaurav Menghani <gauravm@fb.com>
2015-11-10 09:13:51 -0800

    More solutions to the reversing problem.

--- reverse-words.cpp
+++ reverse-words.cpp
@@ -5,9 +5,10 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <sstream>
 using namespace std;
 
-// Have two solutions here.
+// Have multiple solutions here.
 
 // Separate by separator, and join them in the reverse order.
 string solve1(string s) {
@@ -47,6 +48,46 @@ string solve2(string s) {
   return s;
 }
 
+// Assumes a single space between words.
+string solve3(string s) {
+  istringstream iss(s);
+  vector<string> vs;
+  string temp;
+  while (iss >> temp) {
+    vs.push_back(temp);
+  }
+
+  string ret;
+  for (int i = vs.size() - 1; i >= 0; i--) {
+    ret += vs[i];
+    if (i >= 0) {
+      ret += " ";
+    }
+  }
+  return ret;
+}
+
+// Just search for the space, and do something like solve2.
+string solve4(string s) {
+  int prev_pos = 0, find_from = 0;
+  reverse(s.begin(), s.end());
+  while (true) {
+    if (s[find_from] == ' ') {
+      find_from++;
+      continue;
+    }
+
+    size_t pos = s.find(" ", find_from);
+    if (pos == string::npos) {
+      break;
+    }
+    reverse(s.begin() + find_from, s.begin() + pos);
+    find_from = pos;
+  }
+  reverse(s.begin() + find_from, s.end());
+  return s;
+}
+
 int main() {
   char buf[1000];
   string s;
@@ -54,5 +95,7 @@ int main() {
   s = (string)buf;
   cout << solve1(s) << endl;
   cout << solve2(s) << endl;
+  cout << solve3(s) << endl;
+  cout << solve4(s) << endl;
   return 0;
 }
