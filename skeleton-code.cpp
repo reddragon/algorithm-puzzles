f7a366b08858697094b163c2a95dfaf061d80ef4
Gaurav Menghani <gauravm@fb.com>
2016-05-21 09:19:35 -0700

    Adding skeleton code to be used

---
+++ skeleton-code.cpp
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <set>
+#include <vector>
+#include <utility>
+#include <map>
+
+#include "util.h"
+using namespace std;
+
+int main() {
+  return 0;
+}
