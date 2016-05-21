a446b2aae63f199c6ed110fb971c6adcdae5549a
Gaurav Menghani <gauravm@fb.com>
2016-05-21 08:42:49 -0700

    Added more explanation for the multiples problem

--- multiples-of-three-five-seven.cpp
+++ multiples-of-three-five-seven.cpp
@@ -24,6 +24,21 @@ long long nThMultiple(int n) {
   return x;
 }
 
+// There is an O(n) approach to this as well.
+// Maintain three queues, for 3, 5, and 7.
+// Start with 3, 5 and 7 in those queues respectively.
+// Find the minimum of the three queues, and pop it.
+// Let's say you popped number 't'.
+// t would be of the form 3**x . 5**y . 7**z. Let it be denoted by (x,y,z).
+// If z > 0, you can only push (x, y, z+1) into the queue you popped t from.
+// If y > 0, and, z = 0, you can also push (x, y+1, z)
+// If x > 0, y = 0, and, z = 0, you can also push (x+1, y, z).
+// This is to avoid duplication of numbers.
+// As an example, try to draw the recursion tree for getting from
+// (x, y, z) to (x+1, y+1, z+1), with both the current proposed method, and
+// with no restriction on incrementing x, y, z. You would see, with the current
+// method, there is only one possible path.
+
 int main() {
   assert(nThMultiple(1) == 3);
   assert(nThMultiple(1000) == 82046671875);
