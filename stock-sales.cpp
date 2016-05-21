a2247beb1b372a6900c4c928ad65dc932348b1b2
Gaurav Menghani <gauravm@fb.com>
2016-05-21 09:25:58 -0700

    Adding the stock sales problem

---
+++ stock-sales.cpp
@@ -0,0 +1,35 @@
+/**
+ * Given a list of prices of a stock each day, find the maximum profit you can
+ * make by buying and selling the same stock once. 
+ */
+
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <set>
+#include <vector>
+#include <utility>
+#include <map>
+
+#include "util.h"
+using namespace std;
+
+#define INF 1e9
+
+int solve(vector<int> prices) {
+  if (prices.size() == 0) {
+    return 0;
+  }
+  int maxProfit = 0;
+  int minSoFar = INF;
+  for (int i = 0; i < prices.size(); i++) {
+    maxProfit = max(maxProfit, prices[i] - minSoFar);
+    minSoFar = min(minSoFar, prices[i]);
+  }
+  return maxProfit;
+}
+
+int main() {
+  assert(solve({1, 2, 3}) == 2);
+  return 0;
+}
